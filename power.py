7ee2a3a898d8bcb8d4075e42be8ef459c8b69bd5
Tim Parkinson <tsp@ecs.soton.ac.uk>
2013-06-03 14:48:36 +0100

    More sqrt refinements

--- power.py
+++ power.py
@@ -1,7 +1,12 @@
-import sqrt from NumPy as np
+import numpy as np
 
 def power(x):
   return x**2
 
 def square_root(x):
   return np.sqrt(x)
+
+#
+# doesn't work on negative numbers
+#
+# (or strings)
